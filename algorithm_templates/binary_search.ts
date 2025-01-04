1f30a3acb71b14f1b34b55de96a227ac42287036
pshen73 <pshen73@outlook.com>
2025-01-04 18:01:46 +0800

    Refactor binary search and add sorting algorithm
    - Removed binary_search.js
    - Updated binary_search.ts
    - Added sorting.ts

--- algorithm_templates/binary_search.ts
+++ algorithm_templates/binary_search.ts
@@ -1,13 +1,13 @@
-function binarySearchTs<T>(arr: T[], target: T): number {
+function binarySearch<T>(arr: T[], target: T): number {
     if (!arr || arr.length === 0) {
         return -1;
     }
 
-    let left = 0;
-    let right = arr.length - 1;
+    let left: number = 0;
+    let right: number = arr.length - 1;
 
     while (left + 1 < right) {
-        let mid = left + Math.floor((right - left) / 2);
+        let mid: number = left + ((right - left) >> 1);
         if (arr[mid] === target) {
             return mid;
         } else if (arr[mid] < target) {
